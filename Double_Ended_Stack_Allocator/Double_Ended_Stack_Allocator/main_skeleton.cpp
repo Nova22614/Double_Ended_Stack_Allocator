5b2832d25e2aebab6ae9780446fdf6b244cb0222
Max <maxbauer.bauer638@gmail.com>
2021-10-19 21:37:00 +0200

    Tests

--- Double_Ended_Stack_Allocator/Double_Ended_Stack_Allocator/main_skeleton.cpp
+++ Double_Ended_Stack_Allocator/Double_Ended_Stack_Allocator/main_skeleton.cpp
@@ -46,13 +46,178 @@ namespace Tests
 	{
 		void* mem = allocator.Allocate(size, alignment);
 
-		if (mem == nullptr)
+		if (mem != nullptr)
 		{
-			printf("[Error]: Allocator returned nullptr!\n");
-			return false;
+			printf("[AllocationSuccess]: Allocator returned no nullptr!\n");
+			return true;
 		}
 
-		return true;
+		return false;
+	}
+
+	template<class A>
+	bool VerifyAllocationBackSuccess(A& allocator, size_t size, size_t alignment)
+	{
+		void* mem = allocator.AllocateBack(size, alignment);
+
+		if (mem != nullptr)
+		{
+			printf("[AllocationBackSuccess]: Allocator returned no nullptr!\n");
+			return true;
+		}
+
+		return false;
+	}
+
+	template<class A>
+	bool VerifyFreeSuccess(A& allocator, size_t size, size_t alignment)
+	{
+		void* mem = allocator.Allocate(size, alignment);
+		allocator.Free(mem);
+		void* mem2 = allocator.Allocate(size, alignment);
+
+		if (mem == mem2)
+		{
+			printf("[FreeSuccess]: Pointer is the same before and after freeing!\n");
+			return true;
+		}
+
+		return false;
+	}
+
+	template<class A>
+	bool VerifyFreeBackSuccess(A& allocator, size_t size, size_t alignment)
+	{
+		void* mem = allocator.AllocateBack(size, alignment);
+		allocator.FreeBack(mem);
+		void* mem2 = allocator.AllocateBack(size, alignment);
+
+		if (mem == mem2)
+		{
+			printf("[FreeBackSuccess]: Pointer is the same before and after freeing!\n");
+			return true;
+		}
+
+		return false;
+	}
+
+	template<class A>
+	bool VerifyResetSuccess(A& allocator, size_t size, size_t alignment)
+	{
+		void* memFront1 = allocator.Allocate(size, alignment);
+		void* memBack1 = allocator.AllocateBack(size, alignment);
+		allocator.Allocate(size, alignment);
+		allocator.Allocate(size, alignment);
+		allocator.Allocate(size, alignment);
+		allocator.AllocateBack(size, alignment);
+		allocator.AllocateBack(size, alignment);
+		allocator.AllocateBack(size, alignment);
+		allocator.Reset();
+		void* memFront2 = allocator.Allocate(size, alignment);
+		void* memBack2 = allocator.AllocateBack(size, alignment);
+
+
+		if (memBack1 == memBack2 && memFront1 == memFront2)
+		{
+			printf("[ResetSuccess]: Pointers are the same as in the beginning after Reset()!\n");
+			return true;
+		}
+
+		return false;
+	}
+
+	template<class A>
+	bool VerifyMultipleAllocationSuccess(A& allocator, size_t size, size_t alignment)
+	{
+		void* mem1 = allocator.Allocate(size, alignment);
+		void* mem2 = allocator.Allocate(size, alignment);
+
+		if (mem1 != mem2)
+		{
+			printf("[MultipleAllocationSuccess]: Allocator returned two different pointers!\n");
+			return true;
+		}
+
+		return false;
+	}
+
+	template<class A>
+	bool VerifyMultipleAllocationBackSuccess(A& allocator, size_t size, size_t alignment)
+	{
+		void* mem1 = allocator.AllocateBack(size, alignment);
+		void* mem2 = allocator.AllocateBack(size, alignment);
+
+		if (mem1 != mem2)
+		{
+			printf("[MultipleAllocationBackSuccess]: Allocator returned two different pointers!\n");
+			return true;
+		}
+
+		return false;
+	}
+
+	template<class A>
+	bool VerifyAlignmentBackSuccess(A& allocator, size_t size, size_t alignment)
+	{
+		void* mem1 = allocator.AllocateBack(size, alignment);
+		void* mem2 = allocator.AllocateBack(size, alignment);
+
+		if (reinterpret_cast<uintptr_t>(mem1) % alignment == 0 && reinterpret_cast<uintptr_t>(mem2) % alignment == 0)
+		{
+			printf("[AlignmentBackSuccess]: Pointers are aligned\n");
+			return true;
+		}
+
+		return false;
+	}
+
+	template<class A>
+	bool VerifyAlignmentSuccess(A& allocator, size_t size, size_t alignment)
+	{
+		void* mem1 = allocator.Allocate(size, alignment);
+		void* mem2 = allocator.Allocate(size, alignment);
+
+		if (reinterpret_cast<uintptr_t>(mem1) % alignment == 0 && reinterpret_cast<uintptr_t>(mem2) % alignment == 0)
+		{
+			printf("[AlignmentSuccess]: Pointers are aligned\n");
+			return true;
+		}
+
+		return false;
+	}
+
+	template<class A>
+	bool VerifyOverflowSuccess(A& allocator, size_t size, size_t alignment)
+	{
+		void* mem1 = allocator.Allocate(size, alignment);
+		void* mem2 = allocator.Allocate(size, alignment);
+		void* mem3 = allocator.Allocate(size, alignment);
+		void* mem4 = allocator.Allocate(size, alignment);
+
+		if (mem1 != nullptr && mem2 != nullptr && mem3 != nullptr && mem4 == nullptr)
+		{
+			printf("[OverflowSuccess]: Allocator returns nullptr when too much is allocated\n");
+			return true;
+		}
+
+		return false;
+	}
+
+	template<class A>
+	bool VerifyOverflowBackSuccess(A& allocator, size_t size, size_t alignment)
+	{
+		void* mem1 = allocator.AllocateBack(size, alignment);
+		void* mem2 = allocator.AllocateBack(size, alignment);
+		void* mem3 = allocator.AllocateBack(size, alignment);
+		void* mem4 = allocator.AllocateBack(size, alignment);
+
+		if (mem1 != nullptr && mem2 != nullptr && mem3 != nullptr && mem4 == nullptr)
+		{
+			printf("[OverflowBackSuccess]: Allocator returns nullptr when two much is allocated\n");
+			return true;
+		}
+
+		return false;
 	}
 }
 
@@ -82,7 +247,7 @@ private:
 
 
 	//Virtual Memory Size
-	size_t _virtualMemorySize = 1073741824;
+	size_t _virtualMemorySize = 1073741824u;
 
 	//Virtual Memory Creation Pointer
 	void* _firstMemoryElement = 0;
@@ -112,10 +277,10 @@ public:
 		GetSystemInfo(&system_info);
 		_pageSize = system_info.dwPageSize;
 
-		std::cout << _pageSize << std::endl;
-		std::cout << CANARY_SIZE << std::endl;
+		//std::cout << _pageSize << std::endl;
+		//std::cout << CANARY_SIZE << std::endl;
 
-		void* first_Address = VirtualAlloc(NULL, max_size, MEM_RESERVE, PAGE_READWRITE);
+		void* first_Address = VirtualAlloc(NULL, _virtualMemorySize, MEM_RESERVE, PAGE_READWRITE);
 
 		if (!first_Address)
 		{
@@ -139,7 +304,7 @@ public:
 		_frontStackEnd = _begin + _pageSize;
 
 
-		first_Address = VirtualAlloc(reinterpret_cast<void*>(_begin + max_size - _pageSize), _pageSize, MEM_COMMIT, PAGE_READWRITE);
+		first_Address = VirtualAlloc(reinterpret_cast<void*>(_begin + _virtualMemorySize - _pageSize), _pageSize, MEM_COMMIT, PAGE_READWRITE);
 
 		if (!first_Address)
 		{
@@ -168,7 +333,7 @@ public:
 		if (allocatingAddress != _begin)
 		{
 			MetaData* metaData = getMetaData(allocatingAddress);
-			allocatingAddress = allocatingAddress + CANARY_SIZE;
+			allocatingAddress = allocatingAddress + metaData->size + CANARY_SIZE;
 		}
 
 		allocatingAddress = allocatingAddress + sizeof(MetaData) + CANARY_SIZE;
@@ -202,8 +367,11 @@ public:
 		}
 
 		addMetaData(allocatingAddress, _currentFrontAddress, size);
+
+#if WITH_DEBUG_CANARIES
 		addCanary(allocatingAddress - META_SIZE - CANARY_SIZE);
 		addCanary(allocatingAddress + size);
+#endif
 
 		_currentFrontAddress = allocatingAddress;
 
@@ -258,8 +426,11 @@ public:
 			_backStackBegin -= pagesToCommit;
 		}
 		addMetaData(allocatingAddress, _currentBackAddress, size);
-		addCanary(allocatingAddress - sizeof(MetaData) - CANARY_SIZE);
+
+#if WITH_DEBUG_CANARIES
+		addCanary(allocatingAddress - META_SIZE - CANARY_SIZE);
 		addCanary(allocatingAddress + size);
+#endif
 
 		_currentBackAddress = allocatingAddress;
 
@@ -313,14 +484,14 @@ public:
 
 	void Reset(void)
 	{
-		while (_begin != _frontStackEnd)
+		while (_begin != _currentFrontAddress)
 		{
-			Free(reinterpret_cast<void*>(_frontStackEnd));
+			Free(reinterpret_cast<void*>(_currentFrontAddress));
 		}
 
-		while (_end != _backStackBegin)
+		while (_end != _currentBackAddress)
 		{
-			FreeBack(reinterpret_cast<void*>(_backStackBegin));
+			FreeBack(reinterpret_cast<void*>(_currentBackAddress));
 		}
 	}
 
@@ -372,8 +543,25 @@ int main()
 	{
 		// You can remove this, just showcasing how the test functions can be used
 		DoubleEndedStackAllocator allocator(1048576u);
-		Tests::Test_Case_Success("Allocate() returns nullptr", [&allocator]() { return allocator.Allocate(32, 1) == nullptr; }());
-		Tests::Test_Case_Success("Allocate() returns nullptr", [&allocator]() { return allocator.AllocateBack(32, 1) == nullptr; }());
+		Tests::VerifyAllocationSuccess(allocator, 32, 2);
+		Tests::VerifyAllocationBackSuccess(allocator, 32, 2);
+		allocator.Reset();
+		Tests::VerifyFreeSuccess(allocator, 32, 2);
+		Tests::VerifyFreeBackSuccess(allocator, 32, 2);
+		allocator.Reset();
+		Tests::VerifyResetSuccess(allocator, 32, 2);
+		allocator.Reset();
+		Tests::VerifyMultipleAllocationSuccess(allocator, 32, 2);
+		Tests::VerifyMultipleAllocationBackSuccess(allocator, 32, 2);
+		allocator.Reset();
+		Tests::VerifyAlignmentSuccess(allocator, 32, 128);
+		Tests::VerifyAlignmentBackSuccess(allocator, 32, 128);
+		allocator.Reset();
+		Tests::VerifyOverflowSuccess(allocator, 1073741824u / 4, 2);
+		allocator.Reset();
+		DoubleEndedStackAllocator allocator2(1048576u);
+		Tests::VerifyOverflowBackSuccess(allocator2, 1073741824u / 4, 2);
+		allocator.Reset();
 	}
 
 	// You can do whatever you want here in the main function
